ae992327ffd3203c469a44d132f47a121bf1d84a
Kimberly Komatsu <KJKOMATSU@uncg.edu>
2023-03-01 14:39:58 -0500

    Code to find resin bag extract outliers.

---
+++ PBG_resinbag_cleaning.R
@@ -0,0 +1,39 @@
+################################################################################
+##  PBG_resinbag_cleaning.R: Cleaning exported data from resin bag extract assays.
+##
+##  Author: Kimberly Komatsu
+##  Date created: March 1, 2023
+################################################################################
+
+library(tidyverse)
+
+#set working directory
+setwd('C:\\Users\\kjkomatsu\\Dropbox (Smithsonian)\\konza projects\\patch burn')
+
+
+#### import data ####
+n <- read_xlsx('PBG_N_compiled_raw.xlsx') %>% 
+  filter(str_detect(sample, 'U')) %>% 
+  group_by(test, Sample) %>% 
+  filter(dilution==max(dilution)) %>% 
+  ungroup() %>% 
+  mutate(concentration=as.numeric(concentration))
+
+NH4 <- n %>% 
+  filter(test=='KCl Ammonia 10')
+
+NO3 <- n %>% 
+  filter(test=='KCL NO3_NO2 2')
+
+p <- read_xlsx('PBG_P_compiled_raw.xlsx') %>% 
+  filter(str_detect(sample, 'U')) %>% 
+  group_by(test, Sample) %>% 
+  filter(dilution==max(dilution)) %>% 
+  ungroup() %>% 
+  mutate(concentration=as.numeric(concentration))
+
+
+#### find outliers ####
+hist(NH4$concentration)
+hist(NO3$concentration)
+hist(p$concentration)
