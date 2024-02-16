302467e78f04ef212e06df9223726b6a1cf74852
ZacharyBunch <zachbunch2002@gmail.com>
2024-02-16 14:17:36 -0500

    Adding in years since burn info

--- 2023 Invertebrate Herbivory Data.R
+++ 2023 Invertebrate Herbivory Data.R
@@ -15,6 +15,11 @@ library(car)
 
 #### CSV read ####
 Invertebrate_Herbivory_2023 <- read_excel("HerbivoryDataSheetPBG.xlsx")
+BurnInfo2023 <- read_excel("YearsSenseBurned2023.xlsx")
+
+#### BurnInfo Add In ####
+
+Invertebrate_Herbivory_2023 <- left_join(Invertebrate_Herbivory_2023, BurnInfo2023, by = "WS")
 
 #### Data Cleanup ####
 
@@ -22,7 +27,9 @@ Invertebrate_Herbivory_2023 <- Invertebrate_Herbivory_2023 %>% mutate(Treatment 
        Block = ifelse(grepl("S", WS), "North", "South")) %>% 
   unite("Sample", c("WS", "Trans", "Rep"), sep = "_", remove = FALSE) %>% 
   unite("Plant", c("Gensus", "Species"), sep = "_", remove = FALSE) %>% 
-  rename(Herbivory = 'Herbivory (%)' )
+  rename(Herbivory = 'Herbivory (%)' ) %>% 
+  unite("TreatmentSB",c("Treatment","SB"), sep="_") %>% 
+  mutate(Treatment = ifelse(grepl("C1", WS), "ABG", "PBG"))
   
 
 
@@ -64,6 +71,44 @@ EBpalette1 <- c("#228833", "#725DEF", "#EE6677", "#702082")
 EBpalettewoforest <- c("#DC267F", "#785EF0")
 
 ggplot(data=Invertebrate_Herbivory_2023, aes(x=Treatment, y=Herbivory, fill=Plant, color=Plant)) + 
+  geom_violin(aes(fill=Plant, color=Plant,
+                  fill=after_scale(colorspace::lighten(fill, .3))),
+              size=1, bw=.3)
+############################################################
+##### continuous response variable (ABG vs PBG years since burned) #####
+############################################################
+
+#Make it binary
+
+# Define the threshold value
+threshold <- 0.1  # Adjust this threshold as needed
+
+# Convert 'Herbivory (%)' into a binary variable
+Invertebrate_Herbivory_2023$Damage <- ifelse(Invertebrate_Herbivory_2023$Herbivory > threshold, 1, 0)
+
+# Part 1: probability of zero damage
+
+summary(zero_model <- glmer(`Damage` ~ TreatmentSB*Plant + (1|Block),
+                            data = Invertebrate_Herbivory_2023,
+                            family = binomial))
+
+
+Anova(zero_model, type='III') 
+
+# Part 2: distribution of the continuous, non-zero data
+
+Damage_2023 <- Invertebrate_Herbivory_2023 %>% filter(Damage == 1)
+
+summary(cont_model <- lmer(log(Herbivory) ~ TreatmentSB*Plant + (1|Block),
+                           data = Damage_2023))
+Anova(cont_model) 
+back.emmeans(emmeans(cont_model, ~Treatment), transform='log')
+back.emmeans(emmeans(cont_model, ~Plant), transform='log')
+
+
+## Figure ##
+
+ggplot(data=Invertebrate_Herbivory_2023, aes(x=TreatmentSB, y=Herbivory, fill=Plant, color=Plant)) + 
   geom_violin(aes(fill=Plant, color=Plant,
                   fill=after_scale(colorspace::lighten(fill, .3))),
               size=1, bw=.3)
