1e8b84ba57c93ca6e28c1b4e0b44a4eedf55cd85
ZacharyBunch <zachbunch2002@gmail.com>
2024-03-05 15:09:06 -0500

    Updating Weather Data with rainfall

--- 1982-2023 Konza Weather Data.R
+++ 1982-2023 Konza Weather Data.R
@@ -5,7 +5,7 @@ library(readxl)
 
 Weather_Data <- read.csv("AWE012.CSV")
 
-#### Getting Means ####
+#### Getting Temp Means ####
 
 mean_TMAX <- mean(as.numeric(Weather_Data$TMAX), na.rm = T)
 mean_TMIN <- mean(as.numeric(Weather_Data$TMIN), na.rm = T)
@@ -16,3 +16,13 @@ print(mean_TMAX)
 print(mean_TMIN)
 print(mean_TAVE)
 
+#### Getting Average Total Precip Mean ####
+
+Weather_Data$DPPT <- as.numeric(Weather_Data$DPPT, na.rm = T)
+
+# Aggregate the total DPPT for each year
+yearly_DPPT <- aggregate(DPPT ~ RECYEAR, data = Weather_Data, FUN = sum, na.rm = TRUE)
+
+# Calculate the mean of yearly DPPT
+mean_yearly_DPPT <- mean(yearly_DPPT$DPPT)
+print(mean_yearly_DPPT)
